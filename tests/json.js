580b1f16241547929dc459c48af49ee11aa68b68
Lewis Crichton <lewistehminerz@outlook.com>
2018-04-11 10:10:25 +0100

    test(json): Add test for JSON database

--- tests/json.js
+++ tests/json.js
@@ -1,1 +1,42 @@
-import {JSONConnection, JSONTable, JSONKey} from '..';
+const fs = require('fs');
+const {JSONConnection, JSONDatabase, JSONTable, JSONKey} = require('..');
+
+describe('JSON', () => {
+    let connection;
+    let database;
+    it('should error if I am attempting to connect to a non-existant database', done => {
+        new JSONConnection({
+            filePath: './this_does_not_exist.json'
+        }).connect().catch(err => {
+            done();
+        });
+    });
+    it('should return a JSONDatabase object when connecting to an existing database and the Promise resolves', done => {
+        fs.writeFileSync('./sampledb.json', '[]');
+        connection = new JSONConnection({
+            filePath: './sampledb.json'
+        });
+        connection.connect().then(db => {
+            database = db;
+            done();
+        });
+    });
+    it('should add a new table called "test_table"', done => {
+        database.add(new JSONTable({
+            name: 'test_table',
+            keys: []
+        }));
+        connection.save().then(() => {
+            let dataWritten = fs.readFileSync('./sampledb.json', {encoding: 'utf-8'});
+            if (dataWritten !== '["test_table":{}') {
+                throw new Error(dataWritten);
+            }
+            done();
+        });
+    });
+});
+describe('Cleanup', () => {
+    it('delete sampledb', () => {
+        fs.unlinkSync('./sampledb.json');
+    });
+});
